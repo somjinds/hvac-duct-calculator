68e9f76bab8d5bb45a63e880d31aeef5f44d4862
somjinds <jinds2011@gmail.com>
2025-06-02 21:17:27 +0700

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -82,7 +82,7 @@ def main():
             st.error("❌ No suitable square duct size found.")
             return
 
-        st.markdown("#### ✔️ Recommended Duct Sizes")
+        st.markdown("#### ✓ Recommended Duct Sizes")
         if unit_system == "IP":
             st.markdown(f"Based on Q = **{int(Q_input)}** CFM and Target dp = **{dp_input}** inch/100ft.")
         else:
